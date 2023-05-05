b968fbbb1b388774bf6ac6d41c5c4c38381c8a3a
ffserro <ffserro@hotmail.com>
2023-05-05 12:21:24 -0300

    .

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ for single_date in (prm + timedelta(n) for n in range(calendar.monthrange(ano,me
         vermelha.append(single_date)
 
 for i in feriados:
-    if i >= prm and i <= ult:
+    if i >= prm and i <= ult and i not in vermelha:
         vermelha.append(i)
 
 licpag = st.date_input('Qual é o dia da Licença Pagamento? ',value=prm, min_value=prm, max_value=ult)
