30d9fb5a18aa49ceb4c46b36f99843b4f7a524fe
ffserro <ffserro@hotmail.com>
2023-05-05 16:59:59 -0300

    .

--- main.py
+++ main.py
@@ -177,6 +177,6 @@ if mes != 0:
         tabela['A{}'.format(3+i)] = date(ano, mes, i+1).strftime('%d/%m/%y')
         tabela['B{}'.format(3+1)] = date(ano, mes, i+1).weekday()
 
-    st.write(dir(date))
+    st.write(date(2023,05,05).weekday())
     for i in range(30):
         st.write(tabela['A{}'.format(3+i)].value, tabela['B{}'.format(3+i)].value)
