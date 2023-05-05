04cbb93e298dbbdb2450ab00ae8c500538914fae
ffserro <ffserro@hotmail.com>
2023-05-05 17:56:27 -0300

    .

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ if mes != 0:
 
     corrida = []
 
-    nm_ver = list(reversed(div_serv.values()))[list(div_serv.values()).index(v_1):]
+    nm_ver = list(reversed(div_serv.values()))[list(reversed(div_serv.values())).index(v_1):]
     nm_pre = list(div_serv.values())[list(div_serv.values()).index(p_1):]
 
     st.write(nm_ver)
