9ae9d118c55b82f641ac565a864b7530e9220306
ffserro <ffserro@hotmail.com>
2023-05-05 13:31:24 -0300

    .

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ config = data.CalendarConfig(
     legend=False,
 )
 events = [
-    Event('Planning', day='2019-09-23', start='11:00', end='13:00'),
-    Event('Demo', day='2019-09-27', start='15:00', end='16:00'),
-    Event('Retrospective', day='2019-09-27', start='17:00', end='18:00'),
+    Event('Planning', day='2023-05-23', start='11:00', end='13:00'),
+    Event('Demo', day='2023-05-27', start='15:00', end='16:00'),
+    Event('Retrospective', day='2023-05-27', start='17:00', end='18:00'),
 ]
 
 data.validate_config(config)
