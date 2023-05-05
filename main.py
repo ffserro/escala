0e83bdc0f8e89d6e15c3acfb40ae1b65dc39f30a
ffserro <ffserro@hotmail.com>
2023-05-05 14:29:08 -0300

    .

--- main.py
+++ main.py
@@ -16,145 +16,147 @@ st.title('Escala de serviço')
 meses = ['-', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 mes = meses.index(st.selectbox('Escolha o mês da escala a ser visualizada: ', meses))
 
-prm = date(ano, mes, 1)
-ult = date(ano, mes, calendar.monthrange(ano,mes)[-1])
+if mes != 0:
 
-vermelha = []
-preta = []
+    prm = date(ano, mes, 1)
+    ult = date(ano, mes, calendar.monthrange(ano,mes)[-1])
 
-feriados = holidays.Brazil()['{}-01-01'.format(ano): '{}-12-31'.format(ano)]
+    vermelha = []
+    preta = []
 
-for single_date in (prm + timedelta(n) for n in range(calendar.monthrange(ano,mes)[-1])):
-    if single_date.weekday() in (5,6):
-        vermelha.append(single_date)
+    feriados = holidays.Brazil()['{}-01-01'.format(ano): '{}-12-31'.format(ano)]
 
-for i in feriados:
-    if i >= prm and i <= ult:
-        vermelha.append(i)
+    for single_date in (prm + timedelta(n) for n in range(calendar.monthrange(ano,mes)[-1])):
+        if single_date.weekday() in (5,6):
+            vermelha.append(single_date)
 
-for single_date in (prm + timedelta(n) for n in range(calendar.monthrange(ano,mes)[-1])):
-    if single_date not in vermelha:
-        preta.append(single_date)
+    for i in feriados:
+        if i >= prm and i <= ult:
+            vermelha.append(i)
 
-licpag = st.date_input('Qual é o dia da Licença Pagamento? ',value=min(preta), min_value=prm, max_value=ult, key='licpag')
+    for single_date in (prm + timedelta(n) for n in range(calendar.monthrange(ano,mes)[-1])):
+        if single_date not in vermelha:
+            preta.append(single_date)
 
-vermelha.append(licpag)
-preta.remove(licpag)
+    licpag = st.date_input('Qual é o dia da Licença Pagamento? ',value=min(preta), min_value=prm, max_value=ult, key='licpag')
 
-vermelha.sort()
+    vermelha.append(licpag)
+    preta.remove(licpag)
 
-def label_month(year, month, ax, i, j, cl="black"):
-    months = [
-        "Jan",
-        "Fev",
-        "Mar",
-        "Abr",
-        "Mai",
-        "Jun",
-        "Jul",
-        "Ago",
-        "Set",
-        "Out",
-        "Nov",
-        "Dez",
-    ]
+    vermelha.sort()
 
-    month_label = f"{months[month-1]} {year}"
-    ax.text(i, j, month_label, color=cl, va="center")
+    def label_month(year, month, ax, i, j, cl="black"):
+        months = [
+            "Jan",
+            "Fev",
+            "Mar",
+            "Abr",
+            "Mai",
+            "Jun",
+            "Jul",
+            "Ago",
+            "Set",
+            "Out",
+            "Nov",
+            "Dez",
+        ]
 
-
-def label_weekday(ax, i, j, cl="black"):
-    x_offset_rate = 1
-    for weekday in ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"]:
-        ax.text(i, j, weekday, ha="center", va="center", color=cl)
-        i += x_offset_rate
+        month_label = f"{months[month-1]} {year}"
+        ax.text(i, j, month_label, color=cl, va="center")
 
 
-def label_day(ax, day, i, j, cl="black"):
+    def label_weekday(ax, i, j, cl="black"):
+        x_offset_rate = 1
+        for weekday in ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"]:
+            ax.text(i, j, weekday, ha="center", va="center", color=cl)
+            i += x_offset_rate
 
-    ax.text(i, j, int(day), ha="center", va="center", color=cl)
+
+    def label_day(ax, day, i, j, cl="black"):
+
+        ax.text(i, j, int(day), ha="center", va="center", color=cl)
 
 
-def fill_box(ax, i, j):
-    ax.add_patch(
-        patches.Rectangle(
-            (i - 0.5, j - 0.5),
-            1,
-            1,
-            edgecolor="blue",
-            facecolor="red",
-            alpha=0.1,
-            fill=True,
+    def fill_box(ax, i, j):
+        ax.add_patch(
+            patches.Rectangle(
+                (i - 0.5, j - 0.5),
+                1,
+                1,
+                edgecolor="blue",
+                facecolor="red",
+                alpha=0.1,
+                fill=True,
+            )
         )
-    )
 
-def check_color_day(year, month, day, weekday):
-    if date(year, month, day) in holidays.Brazil()['{}-01-01'.format(year): '{}-12-31'.format(year)]:
-        return "red"
+    def check_color_day(year, month, day, weekday):
+        if date(year, month, day) in holidays.Brazil()['{}-01-01'.format(year): '{}-12-31'.format(year)]:
+            return "red"
 
-    if weekday == 6:  # Sunday
-        return "red"
-    
-    if weekday == 5:  # Saturday
-        return "red"
+        if weekday == 6:  # Sunday
+            return "red"
+        
+        if weekday == 5:  # Saturday
+            return "red"
 
-    if date(year, month, day) == licpag:
-        return "red"
+        if date(year, month, day) == licpag:
+            return "red"
 
-    return "black"
-
-
-def month_calendar(ax, year, month, fill):
-    date = datetime(year, month, 1)
-
-    weekday, num_days = monthrange(year, month)
-
-    # adjust by 0.5 to set text at the ceter of grid square
-    x_start = 1 - 0.5
-    y_start = 5 + 0.5
-    x_offset_rate = 1
-    y_offset = -1
-
-    label_month(year, month, ax, x_start, y_start + 2)
-    label_weekday(ax, x_start, y_start + 1)
-
-    j = y_start
-
-    for day in range(1, num_days + 1):
-        i = x_start + weekday * x_offset_rate
-        color = check_color_day(year, month, day, weekday)
-
-        if fill:
-            fill_box(ax, i, j)
-
-        label_day(ax, day, i, j, color)
-        weekday = (weekday + 1) % 7
-        if weekday == 0:
-            j += y_offset
+        return "black"
 
 
-def main(year, month, grid=False, fill=False):
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.axis([0, 7, 0, 7])
-    ax.axis("off")
+    def month_calendar(ax, year, month, fill):
+        date = datetime(year, month, 1)
 
-    if grid:
-        ax.axis("on")
-        ax.grid(grid)
-        for tick in ax.xaxis.get_major_ticks():
-            tick.tick1line.set_visible(False)
-            tick.tick2line.set_visible(False)
-            tick.label1.set_visible(False)
-            tick.label2.set_visible(False)
+        weekday, num_days = monthrange(year, month)
 
-        for tick in ax.yaxis.get_major_ticks():
-            tick.tick1line.set_visible(False)
-            tick.tick2line.set_visible(False)
-            tick.label1.set_visible(False)
-            tick.label2.set_visible(False)
-    month_calendar(ax, year, month, fill)
-    st.pyplot(fig=fig)
+        # adjust by 0.5 to set text at the ceter of grid square
+        x_start = 1 - 0.5
+        y_start = 5 + 0.5
+        x_offset_rate = 1
+        y_offset = -1
+
+        label_month(year, month, ax, x_start, y_start + 2)
+        label_weekday(ax, x_start, y_start + 1)
+
+        j = y_start
+
+        for day in range(1, num_days + 1):
+            i = x_start + weekday * x_offset_rate
+            color = check_color_day(year, month, day, weekday)
+
+            if fill:
+                fill_box(ax, i, j)
+
+            label_day(ax, day, i, j, color)
+            weekday = (weekday + 1) % 7
+            if weekday == 0:
+                j += y_offset
 
 
-main(ano, mes, grid=True, fill=False)
+    def main(year, month, grid=False, fill=False):
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.axis([0, 7, 0, 7])
+        ax.axis("off")
+
+        if grid:
+            ax.axis("on")
+            ax.grid(grid)
+            for tick in ax.xaxis.get_major_ticks():
+                tick.tick1line.set_visible(False)
+                tick.tick2line.set_visible(False)
+                tick.label1.set_visible(False)
+                tick.label2.set_visible(False)
+
+            for tick in ax.yaxis.get_major_ticks():
+                tick.tick1line.set_visible(False)
+                tick.tick2line.set_visible(False)
+                tick.label1.set_visible(False)
+                tick.label2.set_visible(False)
+        month_calendar(ax, year, month, fill)
+        st.pyplot(fig=fig)
+
+
+    main(ano, mes, grid=True, fill=False)
