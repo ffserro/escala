e0792859652eec0b656c91a2dd94b7fb6d577d58
ffserro <ffserro@hotmail.com>
2023-05-05 11:26:40 -0300

    .

--- main.py
+++ main.py
@@ -3,30 +3,17 @@ import calendar
 import holidays
 from datetime import datetime as dt
 
+ano = 2023
 
 st.title('Escala de serviço')
 
 meses = ['-', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-mes = meses.index(st.selectbox('Escolha o mês da escala a ser visualizada: ',meses))
+mes = meses.index(st.selectbox('Escolha o mês da escala a ser visualizada: ', meses))
 
 st.write(mes)
 
-
-st.write(calendar.month(2023, 5))
-
 feriados = holidays.Brazil()
 
-st.write(feriados['2023-01-01': '2023-12-31'])
+st.write(feriados['2023-01-01': '2023-04-31'])
 
-data = st.date_input('Entre com uma data')
-
-try:
-    dts = st.date_input(label='Date Range: ',
-                value=(dt(year=2022, month=5, day=20, hour=16, minute=30), 
-                        dt(year=2022, month=5, day=30, hour=16, minute=30)),
-                key='#date_range',
-                help="The start and end date time")
-    st.write('Start: ', dts[0], "End: ", dts[1])
-
-except:
-    pass
+licpag = st.date_input('Qual é o dia da Licença Pagamento? ')
