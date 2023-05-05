9f596a8de82f452eade5ac3d8fe7ec44ae6a5ea2
ffserro <ffserro@hotmail.com>
2023-05-05 16:58:08 -0300

    .

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ if mes != 0:
 
     for i in range(calendar.monthrange(ano,mes)[-1]):
         tabela['A{}'.format(3+i)] = date(ano, mes, i+1).strftime('%d/%m/%y')
-        tabela['B{}'.format(3+1)] = datetime(ano, mes, i+1).weekday()
+        tabela['B{}'.format(3+1)] = datetime(ano, mes, i+1).weekday
 
     st.write(dir(date))
     for i in range(30):
