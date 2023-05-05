dd65394447bac570d713874b1721c811a0fbb0b1
ffserro <ffserro@hotmail.com>
2023-05-05 11:44:56 -0300

    .

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ st.title('Escala de serviço')
 meses = ['-', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 mes = meses.index(st.selectbox('Escolha o mês da escala a ser visualizada: ', meses))
 
+prm = dt.date(ano, mes, 1)
+ult = dt.date(ano, mes, calendar.monthrange(ano,mes)[-1])
+
 st.write(mes)
 
 feriados = holidays.Brazil()
 
 st.write(feriados['{}-01-01'.format(ano): '{}-12-31'.format(ano)])
 
-licpag = st.date_input('Qual é o dia da Licença Pagamento? ')
+licpag = st.date_input('Qual é o dia da Licença Pagamento? ', value = prm, min_value=prm, max_value=ult)
