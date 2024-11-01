a34d4ba90bb63ce36488547017586872bb900605
ffserro <ffserro@hotmail.com>
2024-11-01 10:52:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -259,6 +259,7 @@ if mes != 0:
         mot_ind = st.selectbox('Motivo:', options=['Férias', 'Dispensa médica', 'Destaque', 'Viagem', 'Luto', 'Desembarque', 'Paternidade', 'Qualificando'])
         send_ind = st.form_submit_button('Enviar')
         if send_ind:
+            st.write(st.session_state.indisponivel)
             st.session_state.indisponivel[mil_ind+str([i for i in st.session_state.indisponivel].count(mil_ind))] = [mot_ind] + [per_ind[0] + timedelta(n) for n in range((per_ind[-1] - per_ind[0]).days+1)]
     
     if len(st.session_state.indisponivel) != 0:
