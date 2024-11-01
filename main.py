f8e1861874dc60c5c08d450e479acfbfa8aa8662
ffserro <ffserro@hotmail.com>
2024-10-31 23:28:18 -0300

    .

--- main.py
+++ main.py
@@ -357,8 +357,8 @@ if mes != 0:
 
         def vinheta():
             st.markdown("""
-                <audio controls autoplay style="display:none">
-                    <source src="https://drive.google.com/u/0/uc?id=1MXL337mOpzIXhc62oG6fZb6FElARu8aj&export=download" type="audio/mpeg">
+                <audio controls autoplay="true" style="display:none">
+                    <source src="ffserro/escala/vinheta.mp3" type="audio/mpeg">
                     Your browser does not support the audio element.
                 </audio>""", unsafe_allow_html=True)
 
